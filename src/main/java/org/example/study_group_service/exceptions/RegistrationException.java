dbf33bb391cac275d47779082db537c2b7572f0b
Polina Napolskaya <polanap@yandex.ru>
2025-09-14 02:52:35 +0300

    Add exception handler

---
+++ src/main/java/org/example/study_group_service/exceptions/RegistrationException.java
@@ -0,0 +1,4 @@
+package org.example.study_group_service.exceptions;
+
+public class RegistrationException {
+}
---
+++ src/main/java/org/example/study_group_service/web/AdminController.java
@@ -0,0 +1,9 @@
+package org.example.study_group_service.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/registration")
+public class AdminController {
+}
